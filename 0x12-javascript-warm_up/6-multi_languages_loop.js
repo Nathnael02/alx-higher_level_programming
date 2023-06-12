2c0a716b89f16100b03144895b352fe8551cdcd8
Nathnael02 <nathnaelyitaferu@yahoo.com>
2023-06-12 07:24:23 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+let myArray = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
 }
